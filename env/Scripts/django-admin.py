3b1bc1acac80e10262b55ebc9829e8880faae35b
PRAJU <prajwalpyadav26@gmail.com>
2024-07-20 20:56:45 +0530

    ok

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\user\onedrive\desktop\fin\ecommerce-django\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
